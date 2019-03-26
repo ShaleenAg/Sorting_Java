f75c2a2f3681b6bd7e947c33313f30dcc526c20a
ShaleenAg <44058516+ShaleenAg@users.noreply.github.com>
2019-03-26 21:34:09 +0530

    NewSort

---
+++ src/NewSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class NewSort {
+
+	public void ns(int[] arr) {
+		Arrays.sort(arr);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NewSort s = new NewSort();
+		int[] a = {1,3,4,5,7,2,8,9,123,345,678};
+		s.ns(a);
+
+	}
+
+}
